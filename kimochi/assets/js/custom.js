f6e69097f18c3be7251d47e919ab5a95c728fcb7
crazychicken@clgt.vn <tuds@Truongs-MacBook-Air.local>
2012-08-13 18:32:04 +0700

    moi sua lai

--- kimochi/assets/js/custom.js
+++ kimochi/assets/js/custom.js
@@ -1,12 +1,27 @@
 (function($){
 	$(document).ready(function(){
+		$('.upload').click(function(){
+			$('.form-upload').css({'display':'block'})
+		});
+		$('.close').click(function(){
+			$('.form-upload').css({'display':'none'})
+		});
+		// zoom img
+		$('.main').click(function(){
+			$('.box-img').removeClass('span12 box-show').addClass('offset2 span8')
+			$('.box-img').show()
+			$('.i-resize-full').removeClass('active')
+		});
 		$('.i-resize-full').toggle(function(){
 			$(this).addClass('active')
-			$(this).parents('.box-show').removeClass('offset1 span10').addClass('span12')
+			$('.box-img').hide()
+			$(this).parents('.box-img').show().removeClass('offset2 span8').addClass('span12 box-show')
 		},function(){
 			$(this).removeClass('active')
-			$(this).parents('.box-show').removeClass('span12').addClass('offset1 span10')
+			$('.box-img').show()
+			$(this).parents('.box-img').removeClass('span12 box-show').addClass('offset2 span8')
 		});
+		//end zoom img
 		$('a[href$="#"]').click(function(e){
 			e.preventDefault();
 		});
