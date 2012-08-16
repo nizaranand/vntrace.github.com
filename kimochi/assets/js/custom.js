0e275beeb2901d6e3c0e5bd083831564ca621945
thanhdang <clgt@clgt.vn>
2012-08-16 22:17:15 +0700

    Update css

--- kimochi/assets/js/custom.js
+++ kimochi/assets/js/custom.js
@@ -1,9 +1,11 @@
 (function($){
 	$(document).ready(function(){
-		$('.upload').click(function(){
+		$('.upload').click(function(e){
+			e.preventDefault();
 			$('.form-upload').css({'display':'block'})
 		});
-		$('.close').click(function(){
+		$('.close').click(function(e){
+			e.preventDefault();
 			$('.form-upload').css({'display':'none'})
 		});
 		// zoom img
@@ -12,11 +14,13 @@
 			$('.box-img').show()
 			$('.i-resize-full').removeClass('active')
 		});
-		$('.i-resize-full').toggle(function(){
+		$('.i-resize-full').toggle(function(e){
+			e.preventDefault();
 			$(this).addClass('active')
 			$('.box-img').hide()
 			$(this).parents('.box-img').show().removeClass('offset2 span8').addClass('span12 box-show')
-		},function(){
+		},function(e){
+			e.preventDefault();
 			$(this).removeClass('active')
 			$('.box-img').show()
 			$(this).parents('.box-img').removeClass('span12 box-show').addClass('offset2 span8')
