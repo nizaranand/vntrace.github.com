a4e705f3133e881be1ddbd36f3c8856fc601fc44
crazychicken@clgt.vn <tuds@Truongs-MacBook-Air.local>
2012-09-08 14:08:16 +0700

    change all

--- tuds-admin/assets/js/custom.js
+++ tuds-admin/assets/js/custom.js
@@ -2,15 +2,18 @@
 	$(document).ready(function(){
 		//page-login
 		$('.help-block a').click(function(){
-			$('#signin').slideUp(300)
-			$('#signup').slideDown(300)
+			$('#signin').slideUp(400)
+			$('#signup').slideDown(400)
 		});
 		$('#signup button').click(function(){
-			$('#signup').slideUp(300)
-			$('#signin').slideDown(300)
+			$('#signup').slideUp(400)
+			$('#signin').slideDown(400)
 		});
+		// end page-login
 
-		$('.navbar-fixed-right a[href="#open"]').click(function(){
+
+		// navbar-fixed-right
+		$('.navbar-fixed-right a[href="#open"]').click(function(e){
 			var a = $(this).parents('.tabbable').find('.tab-content')
 			
 			if(a.hasClass('active')) {
@@ -27,9 +30,11 @@
 		$('.navbar-fixed-right a[href^="#tab"]').click(function(){
 			$(this).parents('.tabbable').find('.tab-content').addClass('active')
 		});
+		// end navbar-fixed-right
 
 
-		$('.list-menu-right').find('a').click(function(e){
+		// list-menu-right
+		$('ul[rel=submenu]').find('a').click(function(e){
             var li = $(this).parent();
             if(li.hasClass('active')) {
                 li.removeClass('active');
@@ -38,7 +43,10 @@
                 li.addClass('active');
             } 
         });
+		// end list-menu-right
 
+
+		// tooltip
         $('.tip-top').tooltip({placement: 'top'});
         $('.tip-left').tooltip({placement: 'left'});
         $('.tip-right').tooltip({placement: 'right'});
@@ -47,5 +55,6 @@
 		$('a[href$="#"]').click(function(e){
 			e.preventDefault();
 		});
+		// end tooltip
 	});
 })(jQuery);
