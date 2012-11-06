977bdf8ca9e1e2d4c68cc628d9ceba0693fcd57f
crazychicken@clgt.vn <tuds@Truongs-MacBook-Air.local>
2012-11-06 18:58:12 +0700

    add8

--- shop-phone/assets/js/custom.js
+++ shop-phone/assets/js/custom.js
@@ -13,20 +13,44 @@
 		});
 		//navbar-search
 		$('a.search').toggle(function(){
-			$('.navbar-search').show()
+			$('.navbar-search').slideDown(100)
 		},function(){
-			$('.navbar-search').hide()
+			$('.navbar-search').slideUp(100)
+		});
+
+		//
+		$('a.search').click(function(e){
+			var a = $(this).parents('.navbar-search')
+			
+			if(a.hasClass('active')) {
+                a.removeClass('active');
+                a.find('.tab-pane').removeClass('active')
+                $(this).parents('.nav-tabs').children().removeClass('active')
+            } else {
+                a.addClass('active');
+            	a.find('.tab-pane:first').addClass('active')
+            	$(this).parent().next().addClass('active')
+            }
 		});
 
 		//new-account
 		$('.new-account').click(function(){
 			$('.register').slideDown()
-		})
+		});
 		// tooltip
         $('.tip-top').tooltip({placement: 'top'});
         $('.tip-left').tooltip({placement: 'left'});
         $('.tip-right').tooltip({placement: 'right'});
         $('.tip-bottom').tooltip({placement: 'bottom'});
+
+        // goto top
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 0) {
+                $('.gotop').fadeIn();
+            } else {
+                $('.gotop').fadeOut();
+            }
+        });
 
 		//end zoom img
 		$('a[href$="#"]').click(function(e){
