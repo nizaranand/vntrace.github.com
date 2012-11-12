29547bff5d00fd0d9f65c5cad731f98f839ccd2f
quocnguyen <quocnguyen@quocnguyen-laptop.(none)>
2012-11-12 21:35:28 +0700

    su dung google webfont loader thay cho link

--- shop-phone/assets/js/custom.js
+++ shop-phone/assets/js/custom.js
@@ -1,3 +1,18 @@
+// asyn google webfont loader
+(function() {
+    WebFontConfig = {
+        google: { families: [ 'Anaheim', 'Audiowide' ] },
+    };
+
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
+
 (function($){
 	$(document).ready(function(){
 		$('a[data-submenu="submenu"]').hover(function(){
