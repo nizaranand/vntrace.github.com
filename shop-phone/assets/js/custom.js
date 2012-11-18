cee0dd497386c47933fa8c3cc69d3aa75e04eb28
crazychicken@clgt.vn <tuds@Truongs-MacBook-Air.local>
2012-11-18 18:32:05 +0700

    test IE7

--- shop-phone/assets/js/custom.js
+++ shop-phone/assets/js/custom.js
@@ -1,20 +1,20 @@
-// asyn google webfont loader
-(function() {
-    WebFontConfig = {
-        google: { families: [ 'Anaheim', 'Audiowide' ] },
-    };
-
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-})();
-
 (function($){
 	$(document).ready(function(){
+        // asyn google webfont loader
+         WebFontConfig = {
+            google: { families: [ 'Anaheim', 'Audiowide' ] }
+          };
+          (function() {
+            var wf = document.createElement('script');
+            wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+              '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+            wf.type = 'text/javascript';
+            wf.async = 'true';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(wf, s);
+          })();
+          // end asyn google webfont loader
+
 		$('a[data-submenu="submenu"]').hover(function(){
 			$(this).next().css({'display':'block'})
 		},function(){
