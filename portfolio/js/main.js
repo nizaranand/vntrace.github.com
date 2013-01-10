6272b08d6d3e12e8faea7306e03a56b6d070240d
Thanh Dang <clgt@clgt.vn>
2013-01-11 06:46:46 +0700

    Update

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -32,6 +32,13 @@ function nav() {
 	/*Get profile container*/
 	$profile = $('#nav .m-profile');
 
+	$sub_hidden_menu = $('.hidden-item-container');
+
+	$last_hidden_menu_item = $('#more-nav-item');
+
+	/*Get init menu item*/
+	$menu_init_item = $menu.find('li').length - 1;
+
 	$(window).bind('resize', function(e){
 		var _nav_inner_width = $nav.innerWidth();
 		var _menu_inner_width = $menu.innerWidth();
@@ -39,6 +46,31 @@ function nav() {
 
 		if((_menu_inner_width + _profile_inner_width) > _nav_inner_width) {
 			console.log('hasOverflow');
+
+			var _sub_width = _menu_inner_width + _profile_inner_width - _nav_inner_width;
+
+			var _i_ = Math.ceil(_sub_width / 55);
+
+			console.log(Math.ceil(_i_));
+
+			if(_i_ > 0) {
+				if($sub_hidden_menu.find('ul li').length === 0) {
+					_i_++;
+					console.log(_i_);
+				}
+
+				for(var i = 0; i < _i_; i++) {
+					/*Get last element*/
+					$sub_hidden_menu.find('ul').prepend($last_hidden_menu_item.prev());
+				}
+
+				$last_hidden_menu_item.show();
+			}
+
+		} else {
+			if($menu_init_item != $menu.find('li').length) {
+
+			}
 		}
 	});
 }
